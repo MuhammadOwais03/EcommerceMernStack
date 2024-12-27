9a4e76b435712ec1e85d02389e02b417725d31fe
MuhammadOwais03 <owaisiqbal2021@gmail.com>
2024-12-27 11:22:42 -0800

    setting basic schemas

---
+++ backend/src/models/product.models.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    images: {
+        type: Array,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    subCategory: {
+        type: String,
+        required: true,
+    },
+    sizes: {
+        type: Array,
+        required: true,
+    },
+    bestSellers: {
+        type: Boolean,
+        default: false,
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Product', ProductSchema);
--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,51 +1,40 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 
-
 const { Schema } = mongoose;
 
-const UserSchema = new Schema ({
-
+const UserSchema = new Schema({
     email: {
-        type:'String',
+        type: 'String',
         required: true,
         unique: true,
     },
-
-    firstName: {
-        type:'String',
+    name: {
+        type: 'String',
         required: true,
     },
-
-    lastName: {
-        type:'String',
-        required: true,
-    },
-
     password: {
         type: 'String',
-        required: 'true',
+        required: true,
     },
-
-
     refreshToken: {
         type: 'String',
-        default: null
+        default: null,
     },
-
     userRole: {
         type: 'String',
-        required: true
-    }
-    
-})
-
+        required: true,
+    },
+    cartData: {
+        type: Object,
+        default: {},
+    },
+}, { minimize: false, timestamps: true });
 
 UserSchema.pre('save', async function (next) {
     if (!this.isModified('password')) {
         return next();
     }
-    
     try {
         this.password = await bcrypt.hash(this.password, 10);
         next();
@@ -54,8 +43,8 @@ UserSchema.pre('save', async function (next) {
     }
 });
 
-userSchema.methods.comparePassword = async function (password) {
+UserSchema.methods.comparePassword = async function (password) {
     return bcrypt.compare(password, this.password);
-  };
+};
 
-
+module.exports = mongoose.model('User', UserSchema);
